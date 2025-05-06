ccf1ecf4567ae4e89549c68625aba2a17408c364
EYF1FE <EYF1FE@FEBO-C-00156>
2025-05-06 14:32:37 +0200

    add Groupcontroller

--- obj/Debug/net9.0/ChatSysBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatSysBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6a26acf940df8f0d035cb6d83c3eae41a870c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66440d3c72f131574b35c895bf2c762c8415c28e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
