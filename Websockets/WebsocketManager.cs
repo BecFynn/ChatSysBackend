756f90372f532a7d7cef9181dd6907e5ca1bd7ec
Olaru Ruben <olr1we@bosch.com>
2025-05-07 13:07:17 +0200

    websockets added

---
+++ Websockets/WebsocketManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Net.WebSockets;
+
+public class WebsocketManager
+{
+    private readonly List<WebSocket> _sockets = new();
+
+    public void AddSocket(WebSocket socket)
+    {
+        _sockets.Add(socket);
+    }
+
+    public async Task BroadcastAsync(string message)
+    {
+        var bytes = Encoding.UTF8.GetBytes(message);
+        var buffer = new ArraySegment<byte>(bytes);
+
+        foreach (var socket in _sockets.ToList())
+        {
+            if (socket.State == WebSocketState.Open)
+            {
+                await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+    }
+
+    public void RemoveClosedSockets()
+    {
+        _sockets.RemoveAll(s => s.State != WebSocketState.Open);
+    }
+}
