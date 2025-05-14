c5782a9967a29807f46d7915939a2d5319242f32
Olaru Ruben <olr1we@bosch.com>
2025-05-14 09:36:48 +0200

    MessageResponse.cs

---
+++ Database/Models/Requests/MessageResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatSysBackend.Database.Models.Requests;
+
+public class MessageResponse
+{
+    public string Action { get; set; }
+    public UserDTO Sender { get; set; }
+    public UserDTO UserReciever { get; set; }
+    public GroupchatDTO GroupReciever { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
