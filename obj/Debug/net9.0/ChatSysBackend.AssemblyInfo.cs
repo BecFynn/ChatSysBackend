625d152f532115b593934dafb68d4a967d171430
Olaru Ruben <olr1we@bosch.com>
2025-05-15 16:02:44 +0200

    changed messages

--- obj/Debug/net9.0/ChatSysBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatSysBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d9e2a952637c6bb8cff327ebd3f05ae679a23b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4597c123c990c9b0445bdaa922cfa976a483ae41")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatSysBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
